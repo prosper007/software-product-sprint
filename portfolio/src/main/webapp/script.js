a38061264de4cf17cdff6b40e5014047bfb29858
Prosper <onungwap@gmail.com>
2020-02-25 12:04:11 -0500

    fetch greeting from server

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getGreeting(){
+    const response = await fetch('/data');
+    const greeting = await response.text();
+    document.getElementById('banner').innerText = greeting;
+}
